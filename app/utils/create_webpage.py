c0eaa63f2f8037b96cf827fb32dda95f2984d37b
Pranjall Kumar <pranjallk1995@gmail.com>
2024-06-25 18:39:33 +0530

    UPDATE: wide reset button

--- app/utils/create_webpage.py
+++ app/utils/create_webpage.py
@@ -17,13 +17,17 @@ class CreateWebpage():
 
     def show_frontend(self) -> None:
         """ module entrypoint """
+
         st.sidebar.title("Book Store")
         st.sidebar.write("\n\n\n\n")
         st.sidebar.divider()
-        reset_button = st.sidebar.button("Reset Database", type="primary")
+        reset_button = st.sidebar.button("Reset Database", type="primary", use_container_width=True)
+        st.sidebar.divider()
+
         if reset_button:
             asyncio.run(self.data_creator.run())
             st.toast("Database set to initial default value")
+
         st.dataframe(
             asyncio.run(self.data_loader.get_dataframe(cfg.DatabaseTables.BOOKS.value)),
             hide_index=True, use_container_width=True
